3bed35e5b4fab8e63c05a227844b085293a38e3a
Krasimir <krasimir.st.tsonev@gmail.com>
2025-09-12 23:56:35 +0300

    Case 09

--- cases/06/Page.tsx
+++ cases/06/Page.tsx
@@ -11,7 +11,6 @@ export default async function Page() {
 
 function EmptyNote() {
   async function createNoteAction() {
-    // Server Function
     "use server";
 
     return await db.notes.create();
